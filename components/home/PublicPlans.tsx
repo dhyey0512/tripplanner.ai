9777a37f3d83624d027188179db2cb092d597343
Dhyey Desai <91964644+dhyey0512@users.noreply.github.com>
2024-11-08 20:54:04 +0530

    Update PublicPlans.tsx removed

--- components/home/PublicPlans.tsx
+++ components/home/PublicPlans.tsx
@@ -1,14 +1,1 @@
-import {getAuthToken} from "@/app/auth";
-import PlanCard from "@/components/dashboard/PlanCard";
-import {api} from "@/convex/_generated/api";
-import {fetchQuery} from "convex/nextjs";
 
-import React from "react";
-
-export default async function PublicPlans() {
-  const token = await getAuthToken();
-
-  const plans = await fetchQuery(api.plan.getPublicPlans, {}, {token});
-
-  return 
-}
