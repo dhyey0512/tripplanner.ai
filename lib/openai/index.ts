80f9dad656274f9ed5444afecbc44282f7550302
Dhyey Desai <91964644+dhyey0512@users.noreply.github.com>
2024-11-08 11:56:47 +0530

    Update index.ts final

--- lib/openai/index.ts
+++ lib/openai/index.ts
@@ -1,28 +1,25 @@
-import {
-  batch1Schema,
-  batch2Schema,
-  batch3Schema
-} from "./schemas";
-
+// index.ts
+import { batch1Schema, batch2Schema, batch3Schema } from "./schemas";
 import OpenAI from "openai";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-const promptSuffix = `generate travel data according to the schema and in JSON format.
-                      Do not return anything in your response outside of curly braces. 
-                      Ensure that all prices are provided in Indian Rupees (₹). 
-                      Dates given, activity preferences, and travel companions should influence the plan by 50%.`;
+const promptSuffix = `generate travel data for hidden gems according to the schema and in JSON format. 
+  Do not return anything in your response outside of curly braces. Ensure that all prices are provided in Indian Rupees (₹). 
+  Dates given, activity preferences, and travel companions should influence the plan by 50%.`;
 
 const callOpenAIApi = (prompt: string, schema: any, description: string) => {
   console.log({ prompt, schema });
   console.log("Calling");
   return openai.chat.completions.create({
-    model: "gpt-3.5-turbo-1106",
+    model: "gpt-4o",
     messages: [
       { role: "system", content: "You are a helpful travel assistant." },
       { role: "user", content: prompt },
     ],
-    functions: [{ name: "set_travel_details", parameters: schema, description }],
+    functions: [
+      { name: "set_travel_details", parameters: schema, description },
+    ],
     function_call: { name: "set_travel_details" },
   });
 };
@@ -30,19 +27,20 @@ const callOpenAIApi = (prompt: string, schema: any, description: string) => {
 export const generatebatch1 = (promptText: string) => {
   const prompt = `${promptText}, ${promptSuffix}`;
   const description = `Generate a description of information about a place according to the following schema:
-  
+
   - About the Place:
     - A string containing at least 50 words about the place.
 
   - Best Time to Visit:
     - A string specifying the best and peak time to visit the place.
-
+    
   Ensure the response follows the schema and is in JSON format.`;
   return callOpenAIApi(prompt, batch1Schema, description);
 };
 
 type OpenAIInputType = {
   userPrompt: string;
+  placeName?: string | undefined;
   activityPreferences?: string[] | undefined;
   fromDate?: number | undefined;
   toDate?: number | undefined;
@@ -51,21 +49,23 @@ type OpenAIInputType = {
 
 export const generatebatch2 = (inputParams: OpenAIInputType) => {
   const description = `Generate recommendations for an adventurous trip according to the following schema:
+
   - Adventures Activities to Do: At least 5 activities, each containing:
-    - Activity name and location
-    - Budget in Indian Rupees (₹)
+      - Activity name and location
+      - Budget in Indian Rupees (₹)
+
   - Local Cuisine Recommendations: At least 5 dishes, each containing:
-    - Name of the dish
-    - Price in Indian Rupees (₹)
+      - Name of the dish
+      - Price in Indian Rupees (₹)
   - Packing Checklist: A list of items to pack based on the weather, activities, and location.
 
-Ensure the response is in JSON format and adheres strictly to the provided schema.`;
-
+  Ensure the response is in JSON format and adheres strictly to the provided schema.`;
   return callOpenAIApi(getPrompt(inputParams), batch2Schema, description);
 };
 
 export const generatebatch3 = (inputParams: OpenAIInputType) => {
-  const description = `Generate a travel itinerary and top places to visit according to the following schema:
+  const { placeName } = inputParams;
+  const description = `Generate a travel itinerary for the hidden gems and extremely rare, unexplored places in ${placeName}, excluding all the famous places, to visit according to the following schema:
 
   - Itinerary:
     - An array with details for each day of the trip, including:
@@ -80,15 +80,20 @@ export const generatebatch3 = (inputParams: OpenAIInputType) => {
   return callOpenAIApi(getPrompt(inputParams), batch3Schema, description);
 };
 
-const getPrompt = ({ userPrompt, activityPreferences, companion, fromDate, toDate }: OpenAIInputType) => {
-  let prompt = `${userPrompt}, from date-${fromDate} to date-${toDate}`;
-
+const getPrompt = ({
+  userPrompt,
+  placeName,
+  activityPreferences,
+  companion,
+  fromDate,
+  toDate,
+}: OpenAIInputType) => {
+  let prompt = `${userPrompt} in ${placeName}, from date-${fromDate} to date-${toDate}`;
   if (companion) prompt += `, travelling with-${companion}`;
 
   if (activityPreferences && activityPreferences.length > 0) {
     prompt += `, activity preferences-${activityPreferences.join(", ")}`;
   }
-
-  prompt = `${prompt}, ${promptSuffix}`;
+  prompt = `${prompt}, generate travel data for hidden gems in ${placeName} ${promptSuffix}`;
   return prompt;
 };
