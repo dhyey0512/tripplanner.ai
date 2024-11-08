593b7e10bc90657ea743df9e237ab1bd29c64535
Dhyey Desai <91964644+dhyey0512@users.noreply.github.com>
2024-11-08 20:11:04 +0530

    Update PublicPlans.tsx final undo

--- components/home/PublicPlans.tsx
+++ components/home/PublicPlans.tsx
@@ -10,5 +10,30 @@ export default async function PublicPlans() {
 
   const plans = await fetchQuery(api.plan.getPublicPlans, {}, {token});
 
-  return 
+  return (
+    <section
+      id="public-plans"
+      className="min-h-[100svh]
+                     bg-background/90
+                     w-full 
+                     flex justify-center items-end
+                     px-5 md:px-0 py-10 md:py-0"
+    >
+      <div className="flex flex-col gap-2">
+        <h2 className="text-yellow-500 text-center text-lg font-bold tracking-wide">
+          Our Community's Favorite Trips
+        </h2>
+        <div
+          className="grid grid-cols-1 
+                      md:grid-cols-2 lg:grid-cols-3
+                      xl:grid-cols-4 4xl:grid-cols-6
+                      gap-2 p-10 justify-center"
+        >
+          {plans?.map((plan) => (
+            <PlanCard key={plan._id} plan={plan} isPublic />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
